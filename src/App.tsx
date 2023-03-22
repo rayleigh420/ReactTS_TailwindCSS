7fcd7f4e655017922aa7f2b9e7853188f36e6880
rayleigh420 <nhatduy0409@gmail.com>
2023-03-22 20:16:37 +0700

    Initial Project iwth TailwindCSS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <div className="App">
+            <h1 className='font-bold'>Hello my name is Le Nhat Duy</h1>
+        </div>
+    )
+}
+
+export default App
