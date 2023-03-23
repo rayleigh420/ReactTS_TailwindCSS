6a50c4d7c9f52261e019fe415f55729a19c7541f
rayleigh420 <nhatduy0409@gmail.com>
2023-03-23 10:07:47 +0700

    Add something

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,4 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
+
+interface SnType {
+    icon?: string,
+    link: string,
+    name: string,
+}
+
 const About = () => {
+    const skill: string[] = ['ReactJS', 'ExpressJS', 'NextJS', 'TypeScript', 'TailwindCSS']
+    const socialNetwork: SnType[] = [
+        { link: 'https://github.com/rayleigh420', name: 'GitHub' },
+        { link: 'https://www.linkedin.com/in/lnduy', name: 'Linkedin' },
+        { link: 'https://www.facebook.com/nhatduy1412.2002', name: 'Facebook' }
+    ]
+
     return (
         <div className="bg-white mx-auto mt-20 mb-16 max-w-7xl px-10">
             <div className="flex flex-row">
@@ -13,7 +29,7 @@ const About = () => {
                         </div>
                         <div className="grow text-center border-b border-r border-b-gray-400 border-r-gray-400 pb-3">
                             <h2 className="font-bold font-mono text-xl">Favourites</h2>
-                            <p className="font-light text-sm text-gray-500">Code, Hentai, Cosplay, Eroge</p>
+                            <p className="font-light text-sm text-gray-500">Coding, Hentai, Cosplay, Eroge</p>
                         </div>
                         <div className="grow text-center border-b border-b-gray-400 pb-3">
                             <h2 className="font-bold font-mono text-xl">School</h2>
@@ -32,12 +48,24 @@ const About = () => {
                         </div>
                         <div className="basis-1/3 text-center pr-[30px]">
                             <h2 className="font-mono font-bold text-xl">Skill</h2>
-                            <span className="inline-block text-xs font-normal text-gray-600 border border-blue-900 px-2 py-1 rounded-lg mr-[6px] mt-[6px] hover:bg-cyan-700 hover:text-white-700">ReactJS</span>
-                            <span className="inline-block text-xs font-normal text-gray-600 border border-blue-900 px-2 py-1 rounded-lg mr-[6px] mt-[6px] hover:bg-cyan-700 hover:text-white-700">ExpressJS</span>
-                            <span className="inline-block text-xs font-normal text-gray-600 border border-blue-900 px-2 py-1 rounded-lg mr-[6px] mt-[6px] hover:bg-cyan-700 hover:text-white-700">NextJS</span>
-                            <span className="inline-block text-xs font-normal text-gray-600 border border-blue-900 px-2 py-1 rounded-lg mr-[6px] mt-[6px] hover:bg-cyan-700 hover:text-white-700">TypeScript</span>
-                            <span className="inline-block text-xs font-normal text-gray-600 border border-blue-900 px-2 py-1 rounded-lg mr-[6px] mt-[6px] hover:bg-cyan-700 hover:text-white-700">Redux</span>
+                            {skill.map((item: string, index: number) => (
+                                <span key={index} className="inline-block text-xs font-normal text-gray-600 border border-blue-900 px-2 py-1 rounded-lg mr-[6px] mt-[6px] hover:bg-cyan-700 hover:text-white-700">{item}</span>
+                            ))}
                         </div>
+                    </div>
+                    <div className="mt-6 ml-12">
+                        <h2 className="font-bold font-mono text-xl mb-4">Social Networks</h2>
+                        {socialNetwork.map((item: SnType, index: number) => (
+                            <span key={index} className="font-semibold text-white bg-gray-400 px-3 py-1 mr-1">
+                                <a href={item.link} target="_blank">{item.name}</a>
+                            </span>
+                        ))}
+                        {/* <span className="font-semibold text-white bg-gray-400 px-3 py-1 mr-1">
+                            <a href="https://github.com/rayleigh420" target="_blank">GitHub</a>
+                        </span>
+                        <span className="font-semibold text-white bg-gray-400 px-3 py-1 mr-1">Linkedin</span>
+                        <span className="font-semibold text-white bg-gray-400 px-3 py-1 mr-1">Facebook</span>
+                        <span className="font-semibold text-white bg-gray-400 px-3 py-1 mr-1">Twiiter</span> */}
                     </div>
 
                 </div>
